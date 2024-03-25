0d6ea5cefd501d0b6f2c00ed10c5569ae4382333
christopher-deriv <christopher@regentmarkets.com>
2024-03-25 12:42:26 +0400

    New lessons and final proj

---
+++ Python for non-Programmers/input.py
@@ -0,0 +1,7 @@
+user_text = input("Enter some text: ")
+user_int = input("Enter 1 to uppercase or 2 to lowercase: ")
+
+if user_int == "1":
+    print(user_text.upper())
+else:
+    print(user_text.lower())
